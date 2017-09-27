783b764cbe8674c6c963bc938064ddb985abd7b3
Ye Lin <erenst2002@hotmail.com>
2017-09-28 09:14:40 +0930

    Commit the branch feat_GST1.2

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -36,6 +36,7 @@ namespace ProductApps
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 totalChargeTextBlock.Text = Convert.ToString(cProduct.TotalPayment + 25);
                 wraptextBlock.Text= Convert.ToString(cProduct.TotalPayment + 25+5);
+                gstTextBlock.Text= Convert.ToString((cProduct.TotalPayment + 25 + 5)*1.1M);
             }
             catch (FormatException)
             {
@@ -51,6 +52,7 @@ namespace ProductApps
             totalPaymentTextBlock.Text = "";
             totalChargeTextBlock.Text = "";
             wraptextBlock.Text = "";
+            gstTextBlock.Text = "";
         }
 
         private void closeButton_Click(object sender, RoutedEventArgs e)
